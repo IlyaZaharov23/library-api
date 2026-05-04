9a7cc4d2ece769cee55f893b198f144d39291105
Elistar_DJ <elistardj@outlook.com>
2026-05-04 19:00:55 +0300

    app init, pg init

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+module.exports = sequelize;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+require("./sentry.instruments");
+const Sentry = require("@sentry/node");
+const express = require("express");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const path = require("path");
+const YAML = require("yamljs");
+const sequelize = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+Sentry.setupExpressErrorHandler(app);
+
+sequelize
+  .authenticate()
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
