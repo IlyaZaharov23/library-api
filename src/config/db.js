c40e58f1f807d81a4fd1abd85236c7cb8837cb14
Elistar_DJ <elistardj@outlook.com>
2026-05-13 16:53:23 +0300

    add db prod connection settings

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,29 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
+const { Sequelize } = require("sequelize");
+
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: process.env.DB_DIALECT,
-  }
-);
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
